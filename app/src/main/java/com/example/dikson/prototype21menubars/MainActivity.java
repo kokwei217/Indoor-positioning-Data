e888b2abc833b393e5ebc90a52543576cd965b33
SebastianJY <jinyee97@gmail.com>
2018-07-10 21:42:31 +0800

    Version with Drawing, GPS(Only longitude) and fantastic UI

--- app/src/main/java/com/example/dikson/prototype21menubars/MainActivity.java
+++ app/src/main/java/com/example/dikson/prototype21menubars/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.dikson.prototype21menubars;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,17 +13,64 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
+
+//    Coordinates Variables
+    ImageView image1;
+
+    StringBuilder sb;
+    int x2, y2, x1, y1, xDiff, yDiff;
+
+    Canvas canvas;
+    Bitmap bitmap, bmp, mutedBitmap;
+    Paint paint, paintOrigin;
+    int bitmapWidth, bitmapHeight, screenWidth, screenHeight;
+
+    boolean setState = true;
+
+//    Drawer Variable
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        screenWidth = displayMetrics.widthPixels;
+        screenHeight = displayMetrics.heightPixels;
+
+        image1 = findViewById(R.id.layout);
+
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paintOrigin = new Paint();
+        paintOrigin.setColor(Color.BLACK);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ioi_layout);
+        bitmapWidth = bitmap.getWidth();
+        bitmapHeight = bitmap.getHeight();
+
+        bitmap = Bitmap.createBitmap(bitmap, 0 , 0 ,bitmapWidth, bitmapHeight);
+        bmp = Bitmap.createScaledBitmap(bitmap, screenWidth, screenHeight, true );
+        mutedBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        canvas = new Canvas(mutedBitmap);
+        image1.setImageBitmap(mutedBitmap);
+        image1.setOnTouchListener(this);
+
+//        Drawer
 
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.T_MainTools);
         setSupportActionBar(toolbar);
@@ -34,13 +86,13 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 //Selected item is highlighted
                 menuItem.setChecked(true);
-//                drawerLayout.closeDrawers();
                 switch (menuItem.getItemId()) {
 
                     case R.id.I_F3:
+                        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.f3_map);
+                        Toast.makeText(MainActivity.this, "Ground Floor", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawers();
                         break;
-
 
                     case R.id.I_WifiList:
                         Intent Wifi = new Intent(MainActivity.this, WifiActivity.class);
@@ -48,11 +100,82 @@ public class MainActivity extends AppCompatActivity {
                         drawerLayout.closeDrawers();
                         break;
 
+                    case R.id.ioi:
+                        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ioi_layout);
+                        drawerLayout.closeDrawers();
+                        break;
 
+                    case R.id.malaysia:
+                        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.malaysia_map);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.campus:
+                        drawerLayout.closeDrawers();
+                        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.campus_map);
+                        Toast.makeText(MainActivity.this, "Level 1", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
                 }
+
+                bitmapWidth = bitmap.getWidth();
+                bitmapHeight = bitmap.getHeight();
+                bitmap = Bitmap.createBitmap(bitmap, 0 , 0 ,bitmapWidth, bitmapHeight);
+                bmp = Bitmap.createScaledBitmap(bitmap, screenWidth, screenHeight, true );
+                mutedBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+                canvas = new Canvas(mutedBitmap);
+                image1.setImageBitmap(mutedBitmap);
                 return true;
             }
         });
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        x2 = (int) event.getX();
+        y2 = (int) event.getY();
+        int action = event.getAction();
+        int xPrev = 0, yPrev = 0;
+
+
+        mutedBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        canvas = new Canvas(mutedBitmap);
+        image1.setImageBitmap(mutedBitmap);
+
+        if(setState){
+            xDiff = 0;
+            yDiff = 0;
+            x1 = (int) event.getX();
+            y1 = (int) event.getY();
+
+            if (action == MotionEvent.ACTION_UP) {
+                setState = false;
+            }
+
+            if (x1 > 0 && y1 > 0 && x1 < mutedBitmap.getWidth() && y1 < mutedBitmap.getHeight()) {
+                canvas.drawCircle(x1, y1, 20, paintOrigin);
+                xPrev = x1;
+                yPrev = y1;
+                sb = new StringBuilder();
+                sb.append("x1:" + x1 + "," + "y1:" + y1);
+//                textView.setText(sb.toString());
+            }
+            else{
+                x1 = xPrev;
+                y1 = yPrev;
+            }
+        }
+        else{
+            if (x2 > 0 && y2 > 0 && x2 < mutedBitmap.getWidth() && y2 < mutedBitmap.getHeight()) {
+                canvas.drawCircle(x1, y1, 20, paintOrigin);
+                canvas.drawCircle(x2, y2, 20, paint);
+                xDiff = x2 - x1;
+                yDiff = y2 - y1;
+                sb = new StringBuilder();
+                sb.append("x1: " + x1 + "," + "y1: " + y1 + "\n"
+                        + "x2: " + xDiff + "," + "y2: " + yDiff);
+            }
+        }
+        return true;
     }
 
     @Override
--- app/src/main/java/com/example/dikson/prototype21menubars/WifiActivity.java
+++ app/src/main/java/com/example/dikson/prototype21menubars/WifiActivity.java
@@ -1,14 +1,21 @@
 package com.example.dikson.prototype21menubars;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -27,22 +34,26 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class WifiActivity extends AppCompatActivity {
-
+//-----------------Toolbar---------------------------
     private DrawerLayout drawerLayout;
 
+//-------------------WIFI----------------------------
     WifiManager wifiManager;
     String[] availableWifiList, bssidList;
     String timeStamp;
@@ -53,18 +64,33 @@ public class WifiActivity extends AppCompatActivity {
     Spinner spinner;
     boolean isEduroamFiltered = false;
 
+//-------------------Location----------------------
+    private final static int ALL_PERMISSIONS_RESULT = 101;
+
+//    TextView tvLatitude, tvLongitude, tvTime;
+    LocationManager locationManager;
+    Location location;
+
+    double latitude, longitude;
+
+    ArrayList<String> permissions;
+    ArrayList<String> permissionsToRequest;
+    ArrayList<String> permissionsRejected = new ArrayList<>();
+
+    boolean canGetLocation = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
 
+//------------------------------------Toolbars--------------------------------------------------------
 //Enables the functions of the custom toolbars
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.T_WifiTools);
         setSupportActionBar(toolbar);
 //Setting the navigation menu button
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         drawerLayout = findViewById(R.id.DL_WifiMenuBar);
 
@@ -91,7 +117,7 @@ public class WifiActivity extends AppCompatActivity {
             }
         });
 
-
+//---------------------------------------------------WIFI-----------------------------------------------------------
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
                 && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
@@ -109,8 +135,58 @@ public class WifiActivity extends AppCompatActivity {
             }
         };
         handler.post(runnable);
+
+//-----------------------------------Location--------------------------------------------
+
+//        tvLatitude = findViewById(R.id.tvLatitude);
+//        tvLongitude = findViewById(R.id.tvLongitude);
+//        tvTime = findViewById(R.id.tvTime);
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+
+        permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        permissionsToRequest = findUnAskedPermissions(permissions);
+
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) &&
+                !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+            //Connection off
+            android.app.AlertDialog.Builder alertDialog = new android.app.AlertDialog.Builder(this);
+            alertDialog.setTitle("Location is Disabled");
+            alertDialog.setMessage("Enable location?");
+            alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+
+            alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                    Toast.makeText(WifiActivity.this, "Location Service is required", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            alertDialog.show();
+
+        } else {
+            //connection on
+            // check permissions
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (permissionsToRequest.size() > 0) {
+                    requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                            ALL_PERMISSIONS_RESULT);
+                    canGetLocation = false;
+                }
+            }
+            getLocation();
+        }
     }
 
+//------------------------------Back Button Function------------------------------------------------
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -120,7 +196,7 @@ public class WifiActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    //  To create the overflow Menu
+//------------------------------Populate the option menu-------------------------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_view, menu);
@@ -128,7 +204,7 @@ public class WifiActivity extends AppCompatActivity {
     }
 
 
-//  Responding to menu item selections
+//-------------------------------Functions for app bar's button------------------------------------
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -137,22 +213,19 @@ public class WifiActivity extends AppCompatActivity {
 //enables the menu button to be clicked
 
             case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+                Intent intent = new Intent(WifiActivity.this,MainActivity.class);
+                startActivity(intent);
                 return true;
 
             case R.id.I_Comment:
-
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
                 alertDialog.setTitle("COMMENTS");
                 alertDialog.setMessage("Enter Remarks");
-
 
                 LinearLayout layout = new LinearLayout(this);
                 layout.setOrientation(LinearLayout.VERTICAL);
 
                 spinner = new Spinner(this);
-//                LinearLayout.LayoutParams LL = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
-//                spinner.setLayoutParams(LL);
                 layout.addView(spinner);
 
                 input = new EditText(this);
@@ -180,20 +253,46 @@ public class WifiActivity extends AppCompatActivity {
 
             case R.id.I_ListAvailable:
                 isEduroamFiltered = false;
+                return true;
+
+            case R.id.I_GPS:
+                AlertDialog.Builder alertDialog_1 = new AlertDialog.Builder(this);
+                alertDialog_1.setTitle("GPS Information");
+
+                LinearLayout layout_1 = new LinearLayout(this);
+                layout_1.setOrientation(LinearLayout.VERTICAL);
+
+                TextView Longitude = new TextView(this);
+                LinearLayout.LayoutParams lp_1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+                Longitude.setLayoutParams(lp_1);
+                layout_1.addView(Longitude);
+                Longitude.setText("Longitude:");
+
+                TextView LongitudeValue = new TextView(this);
+                layout_1.addView(LongitudeValue);
+                LongitudeValue.setText(Double.toString(longitude));
+
+                alertDialog_1.setView(layout_1);
+
+                alertDialog_1.create().show();
+
 
                 return true;
+
+
         }
         return super.onOptionsItemSelected(item);
 
     }
 
+//--------------------------------------------Populating the spinner view----------------------------------------------------------------------------
     public void Spinner_items(){
-
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.Conditions,android.R.layout.simple_spinner_item);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
     }
 
+//------------------------------------------------WIFI------------------------------------------------------------
     public void scanWifi() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
                 && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
@@ -265,11 +364,14 @@ public class WifiActivity extends AppCompatActivity {
         listWifi();
     }
 
+//-------------------------------------------Saving Datas----------------------------------------------
     public void saveData() {
         //FYP BOOK
         String getRemark;
         String getSpinner;
         StringBuilder sb = new StringBuilder();
+        String layoutData = getIntent().getStringExtra("Extra_LayoutData");
+        String gpsData = getIntent().getStringExtra(" Extra_GpsData");
         getRemark = input.getText().toString();
         getSpinner =  spinner.getSelectedItem().toString();
 
@@ -280,7 +382,7 @@ public class WifiActivity extends AppCompatActivity {
             String filePath = file.getAbsolutePath() + "/CollectedData.csv";
             BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
 
-            sb.append(timeStamp + "," + getSpinner + "," + getRemark);
+            sb.append(gpsData + layoutData + "," + timeStamp + "," + getSpinner + getRemark);
             for (int i = 0; i < bssidList.length; i++) {
                 sb.append("," + scanResults.get(i).SSID + " " + scanResults.get(i).BSSID + " " + scanResults.get(i).level);
             }
@@ -295,5 +397,143 @@ public class WifiActivity extends AppCompatActivity {
 
     }
 
+//--------------------------------------Location----------------------------------------------------
+    LocationListener locationListener = new LocationListener() {
+    @Override
+    public void onLocationChanged(Location location) {
+        updateUI(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        getLocation();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+        if (locationManager != null) {
+            //stops location updates when location services are disabled
+            locationManager.removeUpdates(this);
+        }
+
+    }
+};
+
+    private void getLocation(){
+        try {
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null)
+                        updateUI(location);
+                }
+
+            } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location != null)
+                        updateUI(location);
+                }
+            } else {
+                location.setLatitude(0);
+                location.setLongitude(0);
+                updateUI(location);
+            }
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUI(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+//        tvTime.setText(DateFormat.getTimeInstance().format(location.getTime()));
+    }
+
+//--------------------------Location Permissions---------------------------------------------
+
+    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList result = new ArrayList();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean hasPermission(String permission) {
+        if (canAskPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canAskPermission() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case ALL_PERMISSIONS_RESULT:
+                for (String perms : permissionsToRequest) {
+                    if (!hasPermission(perms)) {
+                        permissionsRejected.add(perms);
+                    }
+                }
+
+                if (permissionsRejected.size() > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                            showMessageOKCancel("These permissions are mandatory for the application. Please allow access.",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestPermissions(permissionsRejected.toArray(
+                                                    new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+                } else {
+                    canGetLocation = true;
+                    getLocation();
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.app.AlertDialog.Builder(WifiActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
 }
+
+
+
+
+
+
 
